d55fc7d278a3071bdaa45cc43e4cc60911c6d657
Marie Antons <chefantons@gmail.com>
2020-06-17 15:49:17 -0500

    added import for app

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import './app.scss';
 // There's pros and cons for each way of doing this ...
 import Header from './components/header';
 import Footer from './components/footer';
-import Form from './components/form';
+import Form from './components/form/form';
 
 class App extends React.Component {
   render() {
